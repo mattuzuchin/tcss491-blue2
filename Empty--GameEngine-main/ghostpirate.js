d8849783b75a32bf9b676691d7e1944834da204c
mattuzuchin <mu26@uw.edu>
2025-01-28 14:13:44 -0800

    pulled minh's code and added flipping

--- Empty--GameEngine-main/ghostpirate.js
+++ Empty--GameEngine-main/ghostpirate.js
@@ -7,6 +7,7 @@ class GhostPirate{
         this.width = 40;
         this.height = 40;
         this.speed = 2;
+        this.facingLeft = false;
         this.direction = 1;
         this.animator = new Animator(this.spritesheet, 0, 0, this.width, this.height, 3,  0.1);
         
@@ -14,13 +15,26 @@ class GhostPirate{
     };
     update() {
         this.x += this.speed * this.direction;
-        if(this.x>= this.game.ctx.canvas.width || this.x <= 0) {
-            this.direction *= -1;
+    
+        if (this.x >= this.game.ctx.canvas.width || this.x <= 0) {
+            this.direction *= -1; 
         }
+    
+        this.facingLeft = this.direction === -1;
     }
+    
     draw(ctx) {
         ctx.imageSmoothingEnabled = false;
+        if (this.facingLeft) {
+            ctx.save();
+            ctx.scale(-1, 1); 
+            ctx.translate(-this.x * 2 - this.width, 0); 
+        }
+
         this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        if (this.facingLeft) {
+            ctx.restore();
+        }
     }
 
 }
